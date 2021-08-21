cf58f68d8b1a419cbbc571092d00259c93d04b90
hiroki osame <hiroki.osame@gmail.com>
2021-08-21 14:47:14 -0400

    fix: allow native fs access (#19)

--- tests/fixture/tests/passing-test.js
+++ tests/fixture/tests/passing-test.js
@@ -1,4 +1,5 @@
 import assert from 'assert';
+import fs from 'fs';
 import { last } from 'lodash-es';
 import { add } from '~/add';
 
@@ -13,5 +14,13 @@ it('should load esm', () => {
 	assert(
 		last([1, 2, 3]) === 3,
 		'should be 3',
+	);
+});
+
+it('should access fs', () => {
+	const dirFiles = fs.readdirSync(__dirname);
+	assert(
+		dirFiles.includes('passing-test.js'),
+		'should find self',
 	);
 });
--- tests/fixture/webpack.config.js
+++ tests/fixture/webpack.config.js
@@ -3,6 +3,9 @@ const path = require('path');
 /** @type {import('webpack').Configuration} */
 const baseConfig = {
 	mode: 'production',
+	node: {
+		__dirname: true,
+	},
 	resolve: {
 		alias: {
 			'~': path.resolve(__dirname, 'src/'),
